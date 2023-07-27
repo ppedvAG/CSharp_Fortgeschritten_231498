517daadadff6505c3b1ff5e6235d85d934902f6e
ppedvLukasK <lukask@ppedv.de>
2023-07-27 15:12:19 +0200

    M011 & M012

---
+++ Benchmarks/Program.cs
@@ -0,0 +1,98 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Running;
+using System.Text;
+
+namespace Benchmarks;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		BenchmarkRunner.Run<Benchmarks>();
+		//Benchmarks debuggen
+		//BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig());
+	}
+}
+
+[MemoryDiagnoser(false)]
+[RankColumn]
+//[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+public class Benchmarks
+{
+	public List<Fahrzeug> Fahrzeuge;
+
+	[Params(10000, 50000, 100000)]
+	public int Anzahl;
+
+	[GlobalSetup]
+	public void Setup()
+	{
+		Fahrzeuge = new();
+		Random rnd = new Random();
+		for (int i = 0; i < Anzahl; i++)
+		{
+			Fahrzeug f = new(rnd.Next(100, 500), (FahrzeugMarke) rnd.Next(0, 3));
+			Fahrzeuge.Add(f);
+		}
+	}
+
+	[GlobalCleanup]
+	public void Cleanup()
+	{
+		Fahrzeuge.Clear();
+		Fahrzeuge = null;
+	}
+
+	//[Benchmark]
+	[IterationCount(30)]
+	public void StringPlus()
+	{
+		string s = string.Empty;
+		for (int i = 0; i < 10000; i++)
+			s += i;
+	}
+
+	//[Benchmark]
+	[IterationCount(30)]
+	public void StringB()
+	{
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 10000; i++)
+		{
+			sb.Append(i);
+		}
+		string s = sb.ToString();
+	}
+
+	[Benchmark]
+	[IterationCount(30)]
+	public void ForEach()
+	{
+		List<Fahrzeug> bmwsForEach = new();
+		foreach (Fahrzeug f in Fahrzeuge)
+			if (f.Marke == FahrzeugMarke.BMW)
+				bmwsForEach.Add(f);
+	}
+
+	[Benchmark]
+	[IterationCount(30)]
+	public void StandardLinq()
+	{
+		List<Fahrzeug> bmwsAlt = (from f in Fahrzeuge
+								  where f.Marke == FahrzeugMarke.BMW
+								  select f).ToList();
+	}
+
+	[Benchmark]
+	[IterationCount(30)]
+	public void Linq()
+	{
+		List<Fahrzeug> bmwsNeu = Fahrzeuge.Where(e => e.Marke == FahrzeugMarke.BMW).ToList();
+	}
+}
+
+public record Fahrzeug(int MaxV, FahrzeugMarke Marke);
+
+public enum FahrzeugMarke { Audi, BMW, VW }
---
+++ Sonstiges/Program.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Reflection;
+using System.Text;
+
+namespace Sonstiges;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Wagon a = new Wagon();
+		Wagon b = new Wagon();
+        Console.WriteLine(a == b);
+
+		Zug z = new Zug();
+		z += a;
+		z += b;
+
+		Zug z2 = new Zug();
+		z2++;
+		z2++;
+		z2++;
+		z2++;
+		z += z2;
+
+		foreach (Wagon x in z)
+		{
+
+		}
+
+		Console.WriteLine(z[3]);
+		z[3] = new Wagon();
+
+		Console.WriteLine(z[30, "Rot"]);
+
+		var y = new { ID = 1, Name = "Hans", Adresse = "Zuhaue" };
+        Console.WriteLine(y.ID);
+
+		var anon = z.Wagons.Select(e => new { e.AnzSitze, HC = e.GetHashCode() }); //Mehrere Felder mit Select durch den Code bewegen
+		anon.Where(e => e.HC == 3257973);
+
+		string str = "Ein Text"; //Ein Text im RAM
+		string str2 = "Zwei Text"; //Zwei Text im RAM
+		str += str2; //Kopie im RAM -> Ein TextZwei Text
+		str	+= str2; //Kopie im RAM -> Ein TextZwei TextZwei Text
+					 //Insgesamt 7 Texte
+
+		StringBuilder sb = new StringBuilder();
+		sb.Append(str);
+		sb.Append(str2);
+		sb.Append(str2);
+        Console.WriteLine(sb.ToString()); //Strings werden zwischengespeichert in einer Liste, und am Ende zusammengebaut
+
+		System.Timers.Timer time = new System.Timers.Timer();
+		time.Interval = 1000;
+		time.Start(); //Timer ist ein Task -> Main Thread aufhalten
+
+		Console.ReadKey();
+    }
+}
+
+public class Zug : IEnumerable
+{
+	public List<Wagon> Wagons = new();
+
+	public IEnumerator GetEnumerator()
+	{
+		foreach (Wagon w in Wagons)
+			yield return w; //Speichere die return Werte zwischen und gib sie am Ende der Funktion zurück
+
+		//Selbiges wie oben nur ohne yield
+		//List<Wagon> wagons = new();
+		//foreach (Wagon w in Wagons)
+		//	wagons.Add(w);
+		//return wagons.GetEnumerator();
+
+		//return Wagons.GetEnumerator();
+
+	} //Hier finales return
+
+	public Wagon this[int index]
+	{
+		get => Wagons[index];
+		set => Wagons[index] = value;
+	}
+
+	public Wagon this[int anzSitze, string farbe]
+	{
+		get => Wagons.First(e => e.AnzSitze == anzSitze && e.Farbe == farbe);
+	}
+
+	public static Zug operator +(Zug z, Wagon w)
+	{
+		z.Wagons.Add(w);
+		return z;
+	}
+
+	public static Zug operator +(Zug z, Zug z2)
+	{
+		z.Wagons.AddRange(z2.Wagons);
+		z2.Wagons.Clear();
+		return z;
+	}
+
+	public static Zug operator ++(Zug z)
+	{
+		z.Wagons.Add(new Wagon());
+		return z;
+	}
+}
+
+public class Wagon
+{
+	public int AnzSitze { get; set; }
+
+	public string Farbe { get; set; }
+
+	public static bool operator ==(Wagon a, Wagon b)
+	{
+		return a.AnzSitze == b.AnzSitze && a.Farbe == b.Farbe;
+
+		//foreach (PropertyInfo pi in a.GetType().GetProperties())
+		//	if (pi.GetValue(a) != pi.GetValue(b))
+		//		return false;
+		//return true;
+
+		//return a.GetType().GetProperties().Select(e => e.GetValue(a) == e.GetValue(b)).All(e => e);
+	}
+
+	public static bool operator != (Wagon a, Wagon b)
+	{
+		return !(a == b);
+	}
+}
